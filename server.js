bfd24ae0b1b25a7b93e519b454fd38f2c371a418
Rodesigned <rodesigned@att.net>
2018-03-05 15:56:29 -0500

    updates

--- server.js
+++ server.js
@@ -8,25 +8,24 @@
 // =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
+var path = require("path");
 
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 5000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
-
-// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
-// parse application/json
+app.use(bodyParser.text());
 app.use(bodyParser.json());
-// Static directory -html
-app.use(express.static("app/public"));
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
 
 // Routes
 // =============================================================
-require("./app/routes/api-routes.js")(app);
-require("./app/routes/html-routes.js")(app);
+require('./app/routing/htmlRoutes.js')(app);
+require('./app/routing/apiRoutes.js')(app);
 
 // Starts the server to begin listening
 // =============================================================
